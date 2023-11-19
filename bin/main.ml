6a51715b1b2c35689991bd417c06adc75a902043
bordo <brodylittle011@gmail.com>
2023-11-19 12:19:59 -0500

    playing around

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 open Ray
 
-let r,g,b = 255,255,0
-let i = Ray.Image.create 256 256 (fun x y -> {r;g;b})
+let r,g,b = 0, 30, 30
+let i = Ray.Image.create 1600 900 (fun x y -> {r=abs @@ (int_of_float @@ sqrt(float_of_int @@ x*x + y*y)) mod 255;g;b})
 let () = Ray.Image.to_file "/home/bordo/ray/test/out.ppm" i
